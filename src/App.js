c85be33b0b3390a6af36cc0d6864bc276c8e3b7b
AmineBENCHOUCHE <amine.benchouche@gmail.com>
2023-10-13 10:01:00 +0200

    Project finished

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
-import { useState, useEffect } from "react";
+import { useEffect, useContext } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
-import axios from "axios";
+import BooksContext from "./context/books";
 
 function App() {
-  const [books, setBooks] = useState([]);
+  const { fetchBooks } = useContext(BooksContext);
 
-  const fetchBooks = async () => {
-    const response = await axios.get("http://localhost:3001/books");
-    setBooks(response.data);
-  };
   useEffect(() => {
     fetchBooks();
   }, []);
@@ -17,51 +13,11 @@ function App() {
   // DONT DO THIS because infinite loop of render
   // fetchBooks()
 
-  const editBookById = async (id, newTitle) => {
-    const response = await axios.put(`http://localhost:3001/books/${id}`, {
-      title: newTitle,
-    });
-
-    console.log(response);
-
-    const updatedBooks = books.map((book) => {
-      if (book.id === id) {
-        return { ...book, ...response.data };
-      }
-
-      return book;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const deleteBookById = async (id) => {
-    const response = await axios.delete(`http://localhost:3001/books/${id}`);
-
-    console.log(response);
-    const updatedBooks = books.filter((book) => {
-      return book.id !== id;
-    });
-
-    setBooks(updatedBooks);
-  };
-
-  const createBook = async (title) => {
-    //Make a request to json server
-    const response = await axios.post(" http://localhost:3001/books", {
-      title,
-    });
-
-    // console.log(response);
-    const updatedBooks = [...books, response.data];
-    setBooks(updatedBooks);
-  };
-
   return (
     <div className="app">
       <h1>Reading List</h1>
-      <BookList onEdit={editBookById} books={books} onDelete={deleteBookById} />
-      <BookCreate onCreate={createBook} />
+      <BookCreate />
+      <BookList />
     </div>
   );
 }
